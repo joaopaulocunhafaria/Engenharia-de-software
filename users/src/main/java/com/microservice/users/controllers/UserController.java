d3ec9e052cba79ed2b7e77ecab54f1d4905e4458
João Paulo Debian 17/11/2024 <joaopaulofaria98@gmail.com>
2025-03-12 14:49:15 -0300

    feate: User controller

---
+++ users/src/main/java/com/microservice/users/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.microservice.users.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @GetMapping("/name  ")
+    public String getMethodName() {
+        return "Hello World";
+    }
+    
+}
