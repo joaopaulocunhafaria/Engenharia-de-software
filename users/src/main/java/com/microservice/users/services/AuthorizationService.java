c2875efc9dbde6b3a7d91c418496b2a1023e2d55
João Paulo Debian 17/11/2024 <joaopaulofaria98@gmail.com>
2025-03-13 11:15:22 -0300

    feat: Authorization service for JWT token

--- users/src/main/java/com/microservice/users/services/AuthorizationService.java
+++ users/src/main/java/com/microservice/users/services/AuthorizationService.java
@@ -9,15 +9,18 @@ import org.springframework.stereotype.Service;
 import com.microservice.users.repositories.UserRepository;
 
 @Service
-public class AuthorizationService implements UserDetailsService{
+public class AuthorizationService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username);    
+        var user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + username);
+        }
+        return user;
     }
-    
-    
+
 }
