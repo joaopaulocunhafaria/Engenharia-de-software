ed77cf9b0f6880e811b438694d8ad5c25aaa830c
João Paulo Debian 17/11/2024 <joaopaulofaria98@gmail.com>
2025-03-13 15:38:11 -0300

    Adding products microservice

---
+++ products/src/test/java/com/microservice/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
