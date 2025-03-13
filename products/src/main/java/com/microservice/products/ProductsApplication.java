a9c3ec8c791cb14538767ec0453aa14c8802ef86
João Paulo Debian 17/11/2024 <joaopaulofaria98@gmail.com>
2025-03-13 19:18:33 -0300

    feat: Deleting wrong dependency

--- products/src/main/java/com/microservice/products/ProductsApplication.java
+++ products/src/main/java/com/microservice/products/ProductsApplication.java
@@ -2,8 +2,10 @@ package com.microservice.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class ProductsApplication {
 
 	public static void main(String[] args) {
