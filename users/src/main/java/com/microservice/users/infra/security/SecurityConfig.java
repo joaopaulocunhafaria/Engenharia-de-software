15310833eda590977680db82798021e774fcb99a
João Paulo Debian 17/11/2024 <joaopaulofaria98@gmail.com>
2025-03-13 15:32:01 -0300

    feat: Protecting users controller routes

--- users/src/main/java/com/microservice/users/infra/security/SecurityConfig.java
+++ users/src/main/java/com/microservice/users/infra/security/SecurityConfig.java
@@ -30,8 +30,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(autorize -> autorize
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/users/limitado").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/users/name").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/users/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/users/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
