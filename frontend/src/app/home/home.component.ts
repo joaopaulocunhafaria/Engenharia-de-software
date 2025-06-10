c039961248351e9dc79ffe8337fd793b46ffed10
Alvaro Silva <alvaro.ajsilva@gmail.com>
2025-06-10 13:46:01 -0300

    Pagina home

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  // IMPORTANTE: Adicione esta propriedade products
+  products: any[] = [
+    { id: 1, name: 'nome_produto' },
+    { id: 2, name: 'nome_produto' },
+    { id: 3, name: 'nome_produto' },
+    { id: 4, name: 'nome_produto' },
+    { id: 5, name: 'nome_produto' },
+    { id: 6, name: 'nome_produto' }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    // Aqui você pode carregar os produtos de uma API
+    // this.loadProducts();
   }
 
+  // Método exemplo para carregar produtos
+  loadProducts(): void {
+    // Futuramente, você pode fazer uma chamada HTTP aqui
+    // this.productService.getProducts().subscribe(products => {
+    //   this.products = products;
+    // });
+  }
 }
