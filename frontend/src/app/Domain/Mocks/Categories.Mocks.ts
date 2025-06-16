06a2c11c85d2ee7cdc8b7def4c36234ecd7868ac
João Paulo Debian 17/11/2024 <joaopaulofaria98@gmail.com>
2025-06-16 16:07:21 -0300

    feat: Criando mocks para os produtos e categorias

---
+++ frontend/src/app/Domain/Mocks/Categories.Mocks.ts
@@ -0,0 +1,8 @@
+
+  export const categoriesMocks = [
+    { id: 'all', name: 'Todas as categorias', icon: 'fa-th' },
+    { id: 'livros', name: 'Livros', icon: 'fa-book' },
+    { id: 'notebook', name: 'Notebook', icon: 'fa-laptop' },
+    { id: 'cama-banho', name: 'Cama e banho', icon: 'fa-bed' },
+    { id: 'mobilia', name: 'Mobília', icon: 'fa-couch' }
+  ];
