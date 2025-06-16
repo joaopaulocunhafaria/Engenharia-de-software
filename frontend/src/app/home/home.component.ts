7a55f454fbe368bc91b9b009e3b8b1182d03304c
João Paulo Debian 17/11/2024 <joaopaulofaria98@gmail.com>
2025-06-16 13:02:54 -0300

    feat: Adcionando novos designs para os cards e  imagens novas

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -13,12 +13,68 @@ export class HomeComponent implements OnInit {
   
   // Array de produtos
   products: any[] = [
-    { id: 1, name: 'nome_produto', category: 'livros' },
-    { id: 2, name: 'nome_produto', category: 'notebook' },
-    { id: 3, name: 'nome_produto', category: 'cama-banho' },
-    { id: 4, name: 'nome_produto', category: 'mobilia' },
-    { id: 5, name: 'nome_produto', category: 'livros' },
-    { id: 6, name: 'nome_produto', category: 'notebook' }
+    { id: 1, name: 'O Senhor dos Anéis', category: 'livros', imagePath: 'assets/senhordosaneis.jpg', descricao: 'Uma obra épica de fantasia escrita por J.R.R. Tolkien.', preco: 49.90 },
+    { id: 2, name: 'MacBook Pro', category: 'notebook', imagePath: 'assets/macbookpro.jpg', descricao: 'Notebook de alta performance da Apple.', preco: 12999.90 },
+    { id: 3, name: 'Jogo de Lençóis', category: 'cama-banho', imagePath: 'assets/lencois.jpg', descricao: 'Conjunto de lençóis macios e confortáveis.', preco: 199.90 },
+    { id: 4, name: 'Sofá Retrátil', category: 'mobilia', imagePath: 'assets/sofa.jpg', descricao: 'Sofá retrátil e reclinável para maior conforto.', preco: 2599.90 },
+    { id: 5, name: 'Harry Potter', category: 'livros', imagePath: 'assets/harry.jpg', descricao: 'A famosa série de livros escrita por J.K. Rowling.', preco: 39.90 },
+    { id: 6, name: 'Dell Inspiron', category: 'notebook', imagePath: 'assets/dell.jpg', descricao: 'Notebook versátil e potente da Dell.', preco: 4999.90 },
+
+    { id: 1, name: 'O Senhor dos Anéis', category: 'livros', imagePath: 'assets/senhordosaneis.jpg', descricao: 'Uma obra épica de fantasia escrita por J.R.R. Tolkien.', preco: 49.90 },
+    { id: 2, name: 'MacBook Pro', category: 'notebook', imagePath: 'assets/macbookpro.jpg', descricao: 'Notebook de alta performance da Apple.', preco: 12999.90 },
+    { id: 3, name: 'Jogo de Lençóis', category: 'cama-banho', imagePath: 'assets/lencois.jpg', descricao: 'Conjunto de lençóis macios e confortáveis.', preco: 199.90 },
+    { id: 4, name: 'Sofá Retrátil', category: 'mobilia', imagePath: 'assets/sofa.jpg', descricao: 'Sofá retrátil e reclinável para maior conforto.', preco: 2599.90 },
+    { id: 5, name: 'Harry Potter', category: 'livros', imagePath: 'assets/harry.jpg', descricao: 'A famosa série de livros escrita por J.K. Rowling.', preco: 39.90 },
+    { id: 6, name: 'Dell Inspiron', category: 'notebook', imagePath: 'assets/dell.jpg', descricao: 'Notebook versátil e potente da Dell.', preco: 4999.90 },
+
+    { id: 1, name: 'O Senhor dos Anéis', category: 'livros', imagePath: 'assets/senhordosaneis.jpg', descricao: 'Uma obra épica de fantasia escrita por J.R.R. Tolkien.', preco: 49.90 },
+    { id: 2, name: 'MacBook Pro', category: 'notebook', imagePath: 'assets/macbookpro.jpg', descricao: 'Notebook de alta performance da Apple.', preco: 12999.90 },
+    { id: 3, name: 'Jogo de Lençóis', category: 'cama-banho', imagePath: 'assets/lencois.jpg', descricao: 'Conjunto de lençóis macios e confortáveis.', preco: 199.90 },
+    { id: 4, name: 'Sofá Retrátil', category: 'mobilia', imagePath: 'assets/sofa.jpg', descricao: 'Sofá retrátil e reclinável para maior conforto.', preco: 2599.90 },
+    { id: 5, name: 'Harry Potter', category: 'livros', imagePath: 'assets/harry.jpg', descricao: 'A famosa série de livros escrita por J.K. Rowling.', preco: 39.90 },
+    { id: 6, name: 'Dell Inspiron', category: 'notebook', imagePath: 'assets/dell.jpg', descricao: 'Notebook versátil e potente da Dell.', preco: 4999.90 },
+
+    { id: 1, name: 'O Senhor dos Anéis', category: 'livros', imagePath: 'assets/senhordosaneis.jpg', descricao: 'Uma obra épica de fantasia escrita por J.R.R. Tolkien.', preco: 49.90 },
+    { id: 2, name: 'MacBook Pro', category: 'notebook', imagePath: 'assets/macbookpro.jpg', descricao: 'Notebook de alta performance da Apple.', preco: 12999.90 },
+    { id: 3, name: 'Jogo de Lençóis', category: 'cama-banho', imagePath: 'assets/lencois.jpg', descricao: 'Conjunto de lençóis macios e confortáveis.', preco: 199.90 },
+    { id: 4, name: 'Sofá Retrátil', category: 'mobilia', imagePath: 'assets/sofa.jpg', descricao: 'Sofá retrátil e reclinável para maior conforto.', preco: 2599.90 },
+    { id: 5, name: 'Harry Potter', category: 'livros', imagePath: 'assets/harry.jpg', descricao: 'A famosa série de livros escrita por J.K. Rowling.', preco: 39.90 },
+    { id: 6, name: 'Dell Inspiron', category: 'notebook', imagePath: 'assets/dell.jpg', descricao: 'Notebook versátil e potente da Dell.', preco: 4999.90 },
+
+    { id: 1, name: 'O Senhor dos Anéis', category: 'livros', imagePath: 'assets/senhordosaneis.jpg', descricao: 'Uma obra épica de fantasia escrita por J.R.R. Tolkien.', preco: 49.90 },
+    { id: 2, name: 'MacBook Pro', category: 'notebook', imagePath: 'assets/macbookpro.jpg', descricao: 'Notebook de alta performance da Apple.', preco: 12999.90 },
+    { id: 3, name: 'Jogo de Lençóis', category: 'cama-banho', imagePath: 'assets/lencois.jpg', descricao: 'Conjunto de lençóis macios e confortáveis.', preco: 199.90 },
+    { id: 4, name: 'Sofá Retrátil', category: 'mobilia', imagePath: 'assets/sofa.jpg', descricao: 'Sofá retrátil e reclinável para maior conforto.', preco: 2599.90 },
+    { id: 5, name: 'Harry Potter', category: 'livros', imagePath: 'assets/harry.jpg', descricao: 'A famosa série de livros escrita por J.K. Rowling.', preco: 39.90 },
+    { id: 6, name: 'Dell Inspiron', category: 'notebook', imagePath: 'assets/dell.jpg', descricao: 'Notebook versátil e potente da Dell.', preco: 4999.90 },
+
+    { id: 1, name: 'O Senhor dos Anéis', category: 'livros', imagePath: 'assets/senhordosaneis.jpg', descricao: 'Uma obra épica de fantasia escrita por J.R.R. Tolkien.', preco: 49.90 },
+    { id: 2, name: 'MacBook Pro', category: 'notebook', imagePath: 'assets/macbookpro.jpg', descricao: 'Notebook de alta performance da Apple.', preco: 12999.90 },
+    { id: 3, name: 'Jogo de Lençóis', category: 'cama-banho', imagePath: 'assets/lencois.jpg', descricao: 'Conjunto de lençóis macios e confortáveis.', preco: 199.90 },
+    { id: 4, name: 'Sofá Retrátil', category: 'mobilia', imagePath: 'assets/sofa.jpg', descricao: 'Sofá retrátil e reclinável para maior conforto.', preco: 2599.90 },
+    { id: 5, name: 'Harry Potter', category: 'livros', imagePath: 'assets/harry.jpg', descricao: 'A famosa série de livros escrita por J.K. Rowling.', preco: 39.90 },
+    { id: 6, name: 'Dell Inspiron', category: 'notebook', imagePath: 'assets/dell.jpg', descricao: 'Notebook versátil e potente da Dell.', preco: 4999.90 },
+
+    { id: 1, name: 'O Senhor dos Anéis', category: 'livros', imagePath: 'assets/senhordosaneis.jpg', descricao: 'Uma obra épica de fantasia escrita por J.R.R. Tolkien.', preco: 49.90 },
+    { id: 2, name: 'MacBook Pro', category: 'notebook', imagePath: 'assets/macbookpro.jpg', descricao: 'Notebook de alta performance da Apple.', preco: 12999.90 },
+    { id: 3, name: 'Jogo de Lençóis', category: 'cama-banho', imagePath: 'assets/lencois.jpg', descricao: 'Conjunto de lençóis macios e confortáveis.', preco: 199.90 },
+    { id: 4, name: 'Sofá Retrátil', category: 'mobilia', imagePath: 'assets/sofa.jpg', descricao: 'Sofá retrátil e reclinável para maior conforto.', preco: 2599.90 },
+    { id: 5, name: 'Harry Potter', category: 'livros', imagePath: 'assets/harry.jpg', descricao: 'A famosa série de livros escrita por J.K. Rowling.', preco: 39.90 },
+    { id: 6, name: 'Dell Inspiron', category: 'notebook', imagePath: 'assets/dell.jpg', descricao: 'Notebook versátil e potente da Dell.', preco: 4999.90 },
+
+    { id: 1, name: 'O Senhor dos Anéis', category: 'livros', imagePath: 'assets/senhordosaneis.jpg', descricao: 'Uma obra épica de fantasia escrita por J.R.R. Tolkien.', preco: 49.90 },
+    { id: 2, name: 'MacBook Pro', category: 'notebook', imagePath: 'assets/macbookpro.jpg', descricao: 'Notebook de alta performance da Apple.', preco: 12999.90 },
+    { id: 3, name: 'Jogo de Lençóis', category: 'cama-banho', imagePath: 'assets/lencois.jpg', descricao: 'Conjunto de lençóis macios e confortáveis.', preco: 199.90 },
+    { id: 4, name: 'Sofá Retrátil', category: 'mobilia', imagePath: 'assets/sofa.jpg', descricao: 'Sofá retrátil e reclinável para maior conforto.', preco: 2599.90 },
+    { id: 5, name: 'Harry Potter', category: 'livros', imagePath: 'assets/harry.jpg', descricao: 'A famosa série de livros escrita por J.K. Rowling.', preco: 39.90 },
+    { id: 6, name: 'Dell Inspiron', category: 'notebook', imagePath: 'assets/dell.jpg', descricao: 'Notebook versátil e potente da Dell.', preco: 4999.90 },
+
+    { id: 1, name: 'O Senhor dos Anéis', category: 'livros', imagePath: 'assets/senhordosaneis.jpg', descricao: 'Uma obra épica de fantasia escrita por J.R.R. Tolkien.', preco: 49.90 },
+    { id: 2, name: 'MacBook Pro', category: 'notebook', imagePath: 'assets/macbookpro.jpg', descricao: 'Notebook de alta performance da Apple.', preco: 12999.90 },
+    { id: 3, name: 'Jogo de Lençóis', category: 'cama-banho', imagePath: 'assets/lencois.jpg', descricao: 'Conjunto de lençóis macios e confortáveis.', preco: 199.90 },
+    { id: 4, name: 'Sofá Retrátil', category: 'mobilia', imagePath: 'assets/sofa.jpg', descricao: 'Sofá retrátil e reclinável para maior conforto.', preco: 2599.90 },
+    { id: 5, name: 'Harry Potter', category: 'livros', imagePath: 'assets/harry.jpg', descricao: 'A famosa série de livros escrita por J.K. Rowling.', preco: 39.90 },
+    { id: 6, name: 'Dell Inspiron', category: 'notebook', imagePath: 'assets/dell.jpg', descricao: 'Notebook versátil e potente da Dell.', preco: 4999.90 },
   ];
 
   // Lista de categorias disponíveis
